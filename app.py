89674089e7b5add536919fc816c434809efa46ba
dalton5000 <marius.stieme@gmail.com>
2020-01-14 12:47:33 +0100

    add set

--- app.py
+++ app.py
@@ -5,5 +5,17 @@ app = Flask(__name__)
 def hello():
     return "Hello from Dalton!"
 
+@app.route("/get_set")
+    p_set = get_player_set()
+    return p_set
+
 if __name__ == "__main__":
     app.run()
+
+
+def get_player_set():
+    p_set = []
+    for i in 4:
+        string = "Somename,8,8;1,2,1,2,1,2,1,2,1,2"
+        p_set.append(string)
+    return p_set
